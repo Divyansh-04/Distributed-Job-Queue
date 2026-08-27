017b6bc908b589b668838981caaa9c834b99fb11
Divyansh Goel <divyanshgoel2004@gmail.com>
2026-08-27 23:51:35 +0530

    Added first integration test

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import redis as sync_redis
+import pytest
+
+
+from app.config import REDIS_URL
+import app.queue as queue_module
+
+
+@pytest.fixture(autouse=True)
+def cleanup_redis():
+    sync_client = sync_redis.from_url(REDIS_URL)
+    sync_client.flushdb()
+    yield
+    sync_client.flushdb()
+    sync_client.close()
+
+
+    if queue_module._client is not None:
+        queue_module._client.close()
+        queue_module._client = None
